7ece91e9b179b4a39a05f1083ee38dcee6ac4e1d
Muhammadamin <fahriddinovmuhammadamin34@gmail.com>
2024-08-22 20:12:26 +0500

    Initial commit

---
+++ main.js
@@ -0,0 +1,263 @@
+let obj = [
+    {
+        id: 1,
+        title: "SanDisk SSD PLUS 1TB Internal SSD - SATA III 6 Gb/s",
+        price: 109,
+        description: "Easy upgrade for faster boot up, shutdown, application load and response (As compared to 5400 RPM SATA 2.5” hard drive; Based on published specifications and internal benchmarking tests using PCMark vantage scores) Boosts burst write performance, making it ideal for typical PC workloads The perfect balance of performance and reliability Read/write speeds of up to 535MB/s/450MB/s (Based on internal testing; Performance may vary depending upon drive capacity, host device, OS and application.)",
+        category: "electronics",
+        image: "https://fakestoreapi.com/img/61U7T1koQqL._AC_SX679_.jpg",
+        rating: {
+            rate: 2.9,
+            count: 470
+        }
+    },
+    {
+        id: 2,
+        title: "SanDisk SSD PLUS 1TB Internal SSD - SATA III 6 Gb/s",
+        price: 109,
+        description: "Easy upgrade for faster boot up, shutdown, application load and response (As compared to 5400 RPM SATA 2.5” hard drive; Based on published specifications and internal benchmarking tests using PCMark vantage scores) Boosts burst write performance, making it ideal for typical PC workloads The perfect balance of performance and reliability Read/write speeds of up to 535MB/s/450MB/s (Based on internal testing; Performance may vary depending upon drive capacity, host device, OS and application.)",
+        category: "electronics",
+        image: "https://fakestoreapi.com/img/61U7T1koQqL._AC_SX679_.jpg",
+        rating: {
+            rate: 2.9,
+            count: 470
+        }
+    },
+    {
+        id: 3,
+        title: "SanDisk SSD PLUS 1TB Internal SSD - SATA III 6 Gb/s",
+        price: 109,
+        description: "Easy upgrade for faster boot up, shutdown, application load and response (As compared to 5400 RPM SATA 2.5” hard drive; Based on published specifications and internal benchmarking tests using PCMark vantage scores) Boosts burst write performance, making it ideal for typical PC workloads The perfect balance of performance and reliability Read/write speeds of up to 535MB/s/450MB/s (Based on internal testing; Performance may vary depending upon drive capacity, host device, OS and application.)",
+        category: "electronics",
+        image: "https://fakestoreapi.com/img/61U7T1koQqL._AC_SX679_.jpg",
+        rating: {
+            rate: 2.9,
+            count: 470
+        }
+    },
+    {
+        id: 4,
+        title: "SanDisk SSD PLUS 1TB Internal SSD - SATA III 6 Gb/s",
+        price: 109,
+        description: "Easy upgrade for faster boot up, shutdown, application load and response (As compared to 5400 RPM SATA 2.5” hard drive; Based on published specifications and internal benchmarking tests using PCMark vantage scores) Boosts burst write performance, making it ideal for typical PC workloads The perfect balance of performance and reliability Read/write speeds of up to 535MB/s/450MB/s (Based on internal testing; Performance may vary depending upon drive capacity, host device, OS and application.)",
+        category: "electronics",
+        image: "https://fakestoreapi.com/img/61U7T1koQqL._AC_SX679_.jpg",
+        rating: {
+            rate: 2.9,
+            count: 470
+        }
+    },
+    {
+        id: 5,
+        title: "SanDisk SSD PLUS 1TB Internal SSD - SATA III 6 Gb/s",
+        price: 109,
+        description: "Easy upgrade for faster boot up, shutdown, application load and response (As compared to 5400 RPM SATA 2.5” hard drive; Based on published specifications and internal benchmarking tests using PCMark vantage scores) Boosts burst write performance, making it ideal for typical PC workloads The perfect balance of performance and reliability Read/write speeds of up to 535MB/s/450MB/s (Based on internal testing; Performance may vary depending upon drive capacity, host device, OS and application.)",
+        category: "electronics",
+        image: "https://fakestoreapi.com/img/61U7T1koQqL._AC_SX679_.jpg",
+        rating: {
+            rate: 2.9,
+            count: 470
+        }
+    },
+    {
+        id: 6,
+        title: "SanDisk SSD PLUS 1TB Internal SSD - SATA III 6 Gb/s",
+        price: 109,
+        description: "Easy upgrade for faster boot up, shutdown, application load and response (As compared to 5400 RPM SATA 2.5” hard drive; Based on published specifications and internal benchmarking tests using PCMark vantage scores) Boosts burst write performance, making it ideal for typical PC workloads The perfect balance of performance and reliability Read/write speeds of up to 535MB/s/450MB/s (Based on internal testing; Performance may vary depending upon drive capacity, host device, OS and application.)",
+        category: "electronics",
+        image: "https://fakestoreapi.com/img/61U7T1koQqL._AC_SX679_.jpg",
+        rating: {
+            rate: 2.9,
+            count: 470
+        }
+    },
+    {
+        id: 7,
+        title: "SanDisk SSD PLUS 1TB Internal SSD - SATA III 6 Gb/s",
+        price: 109,
+        description: "Easy upgrade for faster boot up, shutdown, application load and response (As compared to 5400 RPM SATA 2.5” hard drive; Based on published specifications and internal benchmarking tests using PCMark vantage scores) Boosts burst write performance, making it ideal for typical PC workloads The perfect balance of performance and reliability Read/write speeds of up to 535MB/s/450MB/s (Based on internal testing; Performance may vary depending upon drive capacity, host device, OS and application.)",
+        category: "electronics",
+        image: "https://fakestoreapi.com/img/61U7T1koQqL._AC_SX679_.jpg",
+        rating: {
+            rate: 2.9,
+            count: 470
+        }
+    },
+    {
+        id: 8,
+        title: "SanDisk SSD PLUS 1TB Internal SSD - SATA III 6 Gb/s",
+        price: 109,
+        description: "Easy upgrade for faster boot up, shutdown, application load and response (As compared to 5400 RPM SATA 2.5” hard drive; Based on published specifications and internal benchmarking tests using PCMark vantage scores) Boosts burst write performance, making it ideal for typical PC workloads The perfect balance of performance and reliability Read/write speeds of up to 535MB/s/450MB/s (Based on internal testing; Performance may vary depending upon drive capacity, host device, OS and application.)",
+        category: "electronics",
+        image: "https://fakestoreapi.com/img/61U7T1koQqL._AC_SX679_.jpg",
+        rating: {
+            rate: 2.9,
+            count: 470
+        }
+    },
+    {
+        id: 9,
+        title: "SanDisk SSD PLUS 1TB Internal SSD - SATA III 6 Gb/s",
+        price: 109,
+        description: "Easy upgrade for faster boot up, shutdown, application load and response (As compared to 5400 RPM SATA 2.5” hard drive; Based on published specifications and internal benchmarking tests using PCMark vantage scores) Boosts burst write performance, making it ideal for typical PC workloads The perfect balance of performance and reliability Read/write speeds of up to 535MB/s/450MB/s (Based on internal testing; Performance may vary depending upon drive capacity, host device, OS and application.)",
+        category: "electronics",
+        image: "https://fakestoreapi.com/img/61U7T1koQqL._AC_SX679_.jpg",
+        rating: {
+            rate: 2.9,
+            count: 470
+        }
+    },
+    {
+        id: 10,
+        title: "SanDisk SSD PLUS 1TB Internal SSD - SATA III 6 Gb/s",
+        price: 109,
+        description: "Easy upgrade for faster boot up, shutdown, application load and response (As compared to 5400 RPM SATA 2.5” hard drive; Based on published specifications and internal benchmarking tests using PCMark vantage scores) Boosts burst write performance, making it ideal for typical PC workloads The perfect balance of performance and reliability Read/write speeds of up to 535MB/s/450MB/s (Based on internal testing; Performance may vary depending upon drive capacity, host device, OS and application.)",
+        category: "electronics",
+        image: "https://fakestoreapi.com/img/61U7T1koQqL._AC_SX679_.jpg",
+        rating: {
+            rate: 2.9,
+            count: 470
+        }
+    },
+    {
+        id: 11,
+        title: "SanDisk SSD PLUS 1TB Internal SSD - SATA III 6 Gb/s",
+        price: 109,
+        description: "Easy upgrade for faster boot up, shutdown, application load and response (As compared to 5400 RPM SATA 2.5” hard drive; Based on published specifications and internal benchmarking tests using PCMark vantage scores) Boosts burst write performance, making it ideal for typical PC workloads The perfect balance of performance and reliability Read/write speeds of up to 535MB/s/450MB/s (Based on internal testing; Performance may vary depending upon drive capacity, host device, OS and application.)",
+        category: "electronics",
+        image: "https://fakestoreapi.com/img/61U7T1koQqL._AC_SX679_.jpg",
+        rating: {
+            rate: 2.9,
+            count: 470
+        }
+    },
+    {
+        id: 12,
+        title: "SanDisk SSD PLUS 1TB Internal SSD - SATA III 6 Gb/s",
+        price: 109,
+        description: "Easy upgrade for faster boot up, shutdown, application load and response (As compared to 5400 RPM SATA 2.5” hard drive; Based on published specifications and internal benchmarking tests using PCMark vantage scores) Boosts burst write performance, making it ideal for typical PC workloads The perfect balance of performance and reliability Read/write speeds of up to 535MB/s/450MB/s (Based on internal testing; Performance may vary depending upon drive capacity, host device, OS and application.)",
+        category: "electronics",
+        image: "https://fakestoreapi.com/img/61U7T1koQqL._AC_SX679_.jpg",
+        rating: {
+            rate: 2.9,
+            count: 470
+        }
+    },
+    {
+        id: 13,
+        title: "SanDisk SSD PLUS 1TB Internal SSD - SATA III 6 Gb/s",
+        price: 109,
+        description: "Easy upgrade for faster boot up, shutdown, application load and response (As compared to 5400 RPM SATA 2.5” hard drive; Based on published specifications and internal benchmarking tests using PCMark vantage scores) Boosts burst write performance, making it ideal for typical PC workloads The perfect balance of performance and reliability Read/write speeds of up to 535MB/s/450MB/s (Based on internal testing; Performance may vary depending upon drive capacity, host device, OS and application.)",
+        category: "electronics",
+        image: "https://fakestoreapi.com/img/61U7T1koQqL._AC_SX679_.jpg",
+        rating: {
+            rate: 2.9,
+            count: 470
+        }
+    },
+    {
+        id: 14,
+        title: "SanDisk SSD PLUS 1TB Internal SSD - SATA III 6 Gb/s",
+        price: 109,
+        description: "Easy upgrade for faster boot up, shutdown, application load and response (As compared to 5400 RPM SATA 2.5” hard drive; Based on published specifications and internal benchmarking tests using PCMark vantage scores) Boosts burst write performance, making it ideal for typical PC workloads The perfect balance of performance and reliability Read/write speeds of up to 535MB/s/450MB/s (Based on internal testing; Performance may vary depending upon drive capacity, host device, OS and application.)",
+        category: "electronics",
+        image: "https://fakestoreapi.com/img/61U7T1koQqL._AC_SX679_.jpg",
+        rating: {
+            rate: 2.9,
+            count: 470
+        }
+    },
+    {
+        id: 15,
+        title: "SanDisk SSD PLUS 1TB Internal SSD - SATA III 6 Gb/s",
+        price: 109,
+        description: "Easy upgrade for faster boot up, shutdown, application load and response (As compared to 5400 RPM SATA 2.5” hard drive; Based on published specifications and internal benchmarking tests using PCMark vantage scores) Boosts burst write performance, making it ideal for typical PC workloads The perfect balance of performance and reliability Read/write speeds of up to 535MB/s/450MB/s (Based on internal testing; Performance may vary depending upon drive capacity, host device, OS and application.)",
+        category: "electronics",
+        image: "https://fakestoreapi.com/img/61U7T1koQqL._AC_SX679_.jpg",
+        rating: {
+            rate: 2.9,
+            count: 470
+        }
+    },
+    {
+        id: 16,
+        title: "SanDisk SSD PLUS 1TB Internal SSD - SATA III 6 Gb/s",
+        price: 109,
+        description: "Easy upgrade for faster boot up, shutdown, application load and response (As compared to 5400 RPM SATA 2.5” hard drive; Based on published specifications and internal benchmarking tests using PCMark vantage scores) Boosts burst write performance, making it ideal for typical PC workloads The perfect balance of performance and reliability Read/write speeds of up to 535MB/s/450MB/s (Based on internal testing; Performance may vary depending upon drive capacity, host device, OS and application.)",
+        category: "electronics",
+        image: "https://fakestoreapi.com/img/61U7T1koQqL._AC_SX679_.jpg",
+        rating: {
+            rate: 2.9,
+            count: 470
+        }
+    },
+    {
+        id: 17,
+        title: "SanDisk SSD PLUS 1TB Internal SSD - SATA III 6 Gb/s",
+        price: 109,
+        description: "Easy upgrade for faster boot up, shutdown, application load and response (As compared to 5400 RPM SATA 2.5” hard drive; Based on published specifications and internal benchmarking tests using PCMark vantage scores) Boosts burst write performance, making it ideal for typical PC workloads The perfect balance of performance and reliability Read/write speeds of up to 535MB/s/450MB/s (Based on internal testing; Performance may vary depending upon drive capacity, host device, OS and application.)",
+        category: "electronics",
+        image: "https://fakestoreapi.com/img/61U7T1koQqL._AC_SX679_.jpg",
+        rating: {
+            rate: 2.9,
+            count: 470
+        }
+    },
+]
+
+
+let body = document.querySelector('body');
+let ParentBox = document.createElement('div');
+obj.forEach(worker => {
+
+    let box = document.createElement('div');
+    let title = document.createElement('h1');
+    let price = document.createElement('p');
+    let description = document.createElement('p');
+    let category = document.createElement('p');
+    let image = document.createElement('img')
+    let rate = document.createElement('p');
+
+
+    title.textContent = worker.title;
+    price.textContent = worker.price;
+    description.textContent = worker.description;
+    category.textContent = worker.category;
+    image.src = worker.image;
+    rate.textContent = worker.rating.rate;
+
+
+    box.append(title, price, description, category, image, rate);
+    ParentBox.append(box);
+    body.append(ParentBox);
+
+
+
+    ParentBox.style.display = 'flex';
+    ParentBox.style.flexWrap = 'wrap';
+    ParentBox.style.justifyContent = 'space-between';
+    ParentBox.style.width = '1650px';
+    
+    
+    
+    box.style.border = '2px solid white';
+    box.style.width = '350px';
+    box.style.height = '610px';
+    box.style.border = 'solid 2px black';
+    box.style.borderRadius = '10px';
+    box.style.padding = '10px';
+    box.style.margin = '10px';
+    box.style.textAlign = 'center';
+    box.style.display  = 'flex';
+    box.style.flexDirection = 'column';
+    box.style.gap = '20px';
+    box.style.alignItems  = 'center';
+    category.style.fontWeight  = 'bold';
+    image.style.width = '150px';
+    image.style.height = '150px';
+
+
+
+    console.log(box);
+    box.classList.add("allbox")
+});
